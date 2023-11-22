cfb45f9df3bf5c1399228b2c2ef5e6b6e0dfda2f
Viliam B <t3m1nu2@gmail.com>
2023-11-22 23:43:37 +0100

    deepl api

---
+++ examples/deepl-api/03_translate.py
@@ -0,0 +1,45 @@
+#
+#
+
+import io
+import deepl
+import os
+
+env_auth_key = "DEEPL_AUTH_KEY"
+env_server_url = "DEEPL_SERVER_URL"
+
+
+def main() -> None:
+    auth_key = 'e6f7de58-89e5-4cdc-b1cc-918c98e8bc6d:fx' #os.getenv(env_auth_key)
+    server_url = 'https://api-free.deepl.com' # os.getenv(env_server_url)
+    file_name = './test.txt'
+    if auth_key is None:
+        raise Exception(
+            f"Please provide authentication key via the {env_auth_key} "
+            "environment variable or --auth_key argument"
+        )
+
+    # Create a Translator object, and call get_usage() to validate connection
+    translator: deepl.Translator = deepl.Translator(
+        auth_key, server_url=server_url
+    )
+
+    #ginfo: deepl.GlossaryInfo = translator.create_glossary(
+    #    "Test Glossary", "EN", "SK", {"Yes": "Áno"}
+    # )
+    with open(file_name, "rb") as in_file, open(file_name + '.deepl.txt', "wb") as out_file:
+        doc_status: deepl.DocumentStatus = translator.translate_document(
+                in_file,
+                out_file,
+                source_lang="EN",
+                target_lang="SK",
+                filename=file_name,
+                formality=deepl.Formality.DEFAULT,
+        )
+        doc_status.done
+
+    print("Success")
+
+
+if __name__ == "__main__":
+    main()
--- examples/openai-api/speach-to-text/03_translate.py
+++ examples/openai-api/speach-to-text/03_translate.py
@@ -9,18 +9,36 @@
 
 # open file 
 import json
-
-
+import argparse
 from openai import OpenAI
+
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser(description='translate into another language using OpensIA usage --source <filename.txt> --from <Romanian|Russian|English|...>  --to <Romanian|Russian|English|...> ')
+
+# Define arguments with default values
+parser.add_argument('--source',    type=str, default='./build/example.transcript.complete.txt', help='Source text file')
+parser.add_argument('--from',  dest='from_arg',  type=str, default='English', help='translate from language')
+parser.add_argument('--to',        type=str, default='Slovak', choices=['Slovak', 'English'], help='Translate into language')
+parser.add_argument('--block_size',type=int, default=3000,  help='Chunk Block Size for translation')
+parser.add_argument('--model',     type=str, default='gpt-4', help='Model to be used for translation')
+
+# Parse the arguments
+arguments = parser.parse_args()
+arg_source = arguments.source
+arg_from = arguments.from_arg
+arg_to = arguments.to
+arg_block_size = arguments.block_size
+arg_model = arguments.model
+
 client = OpenAI()
 # "You are a helpful assistant that speach Romanian. Your task is to correct any spelling discrepancies in the transcribed text. Only add necessary punctuation such as periods, commas, and capitalization, and use only the context provided."
 
 # adjust 
-system_prompt = "Translate the following Romanian text to Slovak: The text is spoken by female speaker."
+system_prompt = "You are profesional translator from '{}' to {} language. Text that you shall translate was created by academic person that can describe complex thinks wiht symple words, use only the context provided".format(arg_from,arg_to)
 
 def generate_translation(temperature, system_prompt, content):
     response = client.chat.completions.create(
-        model="gpt-4",
+        model=arg_model,
         temperature=temperature,
         messages=[
             {
@@ -38,27 +56,44 @@ def generate_translation(temperature, system_prompt, content):
     return response.choices[0].message.content
 
 corrected_text = ''
-block_size = int(3000)
-file_name = "./build/test.transcript.complete.txt"
+file_name = arg_source
 chunk_count = 1
 # using puthon load text file
 with open(file_name, 'r') as f:
     lines = f.readlines()
     for line in lines:
-        sentences = line.split(',')
-        chunk = ''
-        for sentence in sentences:
-            if len(chunk + sentence) <= block_size:
-                chunk += sentence + ","
-            else:
+        # replace windows vs Linux CR or NL if mixed.
+        line = line.replace('\n','')
+        line = line.replace('\r','')
+        if len(line) <=0 :
+            continue 
+
+        #split big paragraph
+        if len(line) > (2*arg_block_size):
+            sentences = line.split(',')
+            #evaluate sentensed in line (paragraph)
+            chunk = ''
+            for sentence in sentences:
+                if len(sentence) <=0 :
+                    continue            
+                if len(chunk + sentence) <= arg_block_size:
+                    chunk += sentence + ","
+                else:
+                    print("---- chunk {} size {}...".format(chunk_count, len(chunk)))
+                    corrected_text += generate_translation(0, system_prompt, chunk)
+                    chunk = sentence + ","
+                    chunk_count += 1
+        else:
+            # whole line is one chunk
+            chunk = line
+
+        if chunk:
+            if len(chunk) > 0 :
                 print("---- chunk {} size {}...".format(chunk_count, len(chunk)))
                 corrected_text += generate_translation(0, system_prompt, chunk)
-                chunk = sentence + ","
                 chunk_count += 1
-        if chunk:
-            print("---- chunk {} size {}...".format(chunk_count, len(chunk)))
-            corrected_text += generate_translation(0, system_prompt, chunk)
-            chunk_count += 1
+
+        corrected_text += "\n"
 
 print("writing result size {}...\n".format(len(corrected_text)))
 with open(file_name + '.translate.txt', 'w') as ftxt:
